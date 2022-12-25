81b992113be590372cd138f29c12dc5f22ce066c
Igor Olkhov <smartestbox@gmail.com>
2022-12-26 00:33:52 +0100

    Setup task Generate Random Whole Numbers within a Range

--- app.js
+++ app.js
@@ -77,3 +77,12 @@ function randomWholeNum() {
 }
 
 // ---------------------------------------------------------------------------
+
+// Basic JS. Task - Generate Random Whole Numbers within a Range
+// Create a function called randomRange that takes a range myMin and myMax and 
+// returns a random whole number that's greater than or equal to myMin, and 
+// is less than or equal to myMax, inclusive.
+
+function randomRange(myMin, myMax) {
+  return 0;
+}
