671a84fc829db073d5f11d8c3e6834c44d9efbeb
Igor Olkhov <smartestbox@gmail.com>
2022-12-26 00:50:20 +0100

    Setup task Use the parseInt Function

--- app.js
+++ app.js
@@ -88,3 +88,11 @@ function randomRange(myMin, myMax) {
 }
 
 // ---------------------------------------------------------------------------
+
+// Basic JS. Task - Use the parseInt Function
+// Use parseInt() in the convertToInteger function 
+// so it converts the input string str into an integer, and returns it.
+
+function convertToInteger(str) {
+
+}
