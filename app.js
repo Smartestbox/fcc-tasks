e46c516ca1443b51ca32f4dccc84923e3b54e474
Igor Olkhov <smartestbox@gmail.com>
2022-12-26 01:05:35 +0100

    Setup task Use Multiple Conditional (Ternary) Operators

--- app.js
+++ app.js
@@ -120,3 +120,17 @@ function checkEqual(a, b) {
 
 // ---------------------------------------------------------------------------
 
+// Basic JS. Task - Use Multiple Conditional (Ternary) Operators
+// In the checkSign function, use multiple conditional operators - 
+// following the recommended format used in findGreaterOrEqual - 
+// function findGreaterOrEqual(a, b) {
+//   return (a === b) ? "a and b are equal" 
+//     : (a > b) ? "a is greater" 
+//     : "b is greater";
+// }
+// to check if a number is positive, negative or zero. The function should return 
+// positive, negative or zero.
+
+function checkSign(num) {
+
+}
